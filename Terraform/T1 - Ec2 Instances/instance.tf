d1cbb34684fd1e72b9e86d979fe61a56ddff8744
Ankit Solanki <ankitsolanki1821@gmail.com>
2024-05-14 13:02:33 +0530

    Initial Commit

--- Terraform/T1 - Ec2 Instances/instance.tf
+++ Terraform/T1 - Ec2 Instances/instance.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "worker_node" {
   count         = 3
   ami           = "ami-04b70fa74e45c3917" 
   instance_type = "t2.medium"  
-  security_groups = [ "sg-0cbc52801035b93ec" ]
+  security_groups = ["sg-0cbc52801035b93ec"]
+  vpc = "vpc-0b57dda94f28d36b3"
 
 
   tags = {
