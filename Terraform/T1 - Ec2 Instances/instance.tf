8d4b3dbbb01179920c55596dd47260ab902b5a29
Ankit Solanki <ankitsolanki1821@gmail.com>
2024-05-14 12:56:34 +0530

    Initial Commit

--- Terraform/T1 - Ec2 Instances/instance.tf
+++ Terraform/T1 - Ec2 Instances/instance.tf
@@ -1,9 +1,9 @@
 # Create an EC2 instance in the public subnet
 resource "aws_instance" "worker_node" {
   count         = 3
-  ami           = var.ami 
+  ami           = "ami-04b70fa74e45c3917" 
   instance_type = "t2.medium"  
-  security_groups = var.sg-id
+  security_groups = [ "sg-0cbc52801035b93ec" ]
 
 
   tags = {
