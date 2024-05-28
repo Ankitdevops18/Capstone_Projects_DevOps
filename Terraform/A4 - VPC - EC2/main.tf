6258dc1edc887d8dd3726c18df356b3b4a2158c5
Ankit Solanki <ankitsolanki1821@gmail.com>
2024-05-28 14:19:24 +0530

    Pushing Terraform Assignments files

--- Terraform/A4 - VPC - EC2/main.tf
+++ Terraform/A4 - VPC - EC2/main.tf
@@ -50,51 +50,13 @@ resource "aws_route_table_association" "test-rt-sub-assoc" {
     route_table_id = aws_route_table.test-rt.id
 }
 
-# Creating a Security Group
-resource "aws_security_group" "test-sg" {
-    name = "test-sg"
-    description = "Enable web traffic for the project"
-    vpc_id = aws_vpc.test-vpc.id
-    ingress {
-        description = "HTTPS traffic"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        description = "HTTP traffic"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        description = "SSH port"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    tags = {
-        Name = "test-sg1"
-    }
-}
 
 
 # Creating an Ubuntu EC2 instance
 resource "aws_instance" "test-instance" {
     ami = "ami-09040d770ffe2224f"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.test-sg.id]
-
+    vpc_id = aws_vpc.test-vpc.id
 
     tags = {
         Name = "Assignment4-instance"
