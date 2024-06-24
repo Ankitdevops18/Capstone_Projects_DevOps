d32638c54f6c88f606f62d1b8aaff4f1ea64a44d
Ankit Solanki <ankitsolanki1821@gmail.com>
2024-06-24 09:36:13 +0530

    Adding some Linux Scripts & Python files

--- Python/api_calls_http_client.py
+++ Python/api_calls_http_client.py
@@ -39,4 +39,87 @@ def add_asset( scopeId,
 path,
 type_
 ):
+  print(scopeId,path,type_)
+
+def read_file(file_path):
+  return open(file_path, mode=r, encodig="utf-8").read()
+
+
+def sync_asset_data():
+ # APT _DIR_ FOLDER - "APIs"
+ # apis_dir_names - os. listdir(APT_DIR_FOLDER)
+
+  lang = "{{ language }}"
+  LANG_DIR = "{{ base_path }}" + "/" + "{{ api_folder }}"
+  OVERVIEW_DIR = LANG_DIR + "/" + "Overview"
+  ATTACHMENT_DIR = LANG_DIR + "/" + "attachments"
+  IMAGE_DIR = LANG_DIR + "/" + "images" 
   
+  overviews = os.listdir(OVERVIEW_DIR)
+
+  print(OVERVIEW_DIR)
+  print (ATTACHMENT_DIR)
+  print (IMAGE_DIR)
+
+  # For each api with in the set, get the overview data
+  for overview in overviews:
+    CONTENT_SRC_PATH = OVERVIEW_DIR + "/" + overview
+    print(CONTENT_SRC_PATH)
+    apiSpecId = "{{ scope_id }}"
+    print(apiSpecId)
+
+  #Get Asset Type
+  if ".txt" in overview :
+    asset_type - "ApiOverviewParent"
+  elif "specs.yaml" in overview :
+    asset_type - "ApiSpec"
+  elif "documentation.md" in overview :
+    asset_type - "DocumentationMarkdown"
+  elif "overview.md" in overview :
+    asset_type - "OverviewMarkdown"
+  else :
+    asset_type = "APIOverview"
+
+  #Getting File Path file_path -
+  file_path = "/apis/" + "{{ api_folder }}" + "overview" + "/"+ overview
+
+
+
+# delete_all_by_scope_id(access_token,apiSpecId)
+#_add_asset (scopeld, path, type_, data, permissionType,langauge,content, assetName):
+
+
+  # upload overview files
+  overview_filecontent = read_file(CONTENT_SRC_PATH)
+
+  ovparent_result = add_asset(
+  apiSpecId,
+  file_path,
+  asset_type,
+  overview_filecontent,
+  "Internal",
+  lang,
+  overview_filecontent,
+  overview
+  )
+
+  # upload all attachments get id
+  if os.path.isdir(ATTACHMENT_DIR):
+    attachments = os.listdir (ATTACHMENT_DIR)
+    for attachment in attachments:
+      attachment_file_path = ATTACHMENT_DIR + "/" + attachment
+      attachment_result = add_asset(
+        apiSpecId,
+        "/" +"{f api_folder }}" + "/" + "attachments" + "/" + "Attachment",
+        "Internal", 
+        lang,
+        "download-only", 
+        attachment, 
+        attachment_file_path,
+      )
+
+def main():
+  sync_asset_data()
+
+
+main()
---
+++ Python/sum.py
@@ -0,0 +1,12 @@
+number = range(1,100)
+sum = 0
+
+for num in number:
+    sum = sum + num
+    
+print(sum)
+
+
+
+
+
